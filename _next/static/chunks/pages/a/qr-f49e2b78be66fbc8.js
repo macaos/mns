de0efe471a655a6d12ea644f1ac08fbc79ea88fa
jay-kiboko <jay@kiboko.ai>
2023-07-18 01:04:41 +0900

    deploytest

---
+++ _next/static/chunks/pages/a/qr-f49e2b78be66fbc8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[719],{3519:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/a/qr",function(){return _(9627)}])},9627:function(n,u,_){"use strict";_.r(u);var t=_(5893);_(7294);let e=()=>(0,t.jsx)("div",{children:"d"});u.default=e}},function(n){n.O(0,[774,888,179],function(){return n(n.s=3519)}),_N_E=n.O()}]);
